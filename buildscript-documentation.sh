60499f124d7a6b8701c13769fbe4df66d74a4ca4
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2020-12-16 08:15:46 +0100

    First steps at documentation

---
+++ buildscript-documentation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sphinx-build scripts/sphinx docs
---
+++ scripts/ofmx.py
@@ -0,0 +1,17 @@
+"""
+ofmx.py
+====================================
+Tools to interpret ofmx files
+"""
+
+def about_me(your_name):
+    """
+    Return the most important thing about a person.
+
+    Parameters
+    ----------
+    your_name
+        A string indicating the name of the person.
+
+    """
+    return "The wise {} loves Python.".format(your_name)
