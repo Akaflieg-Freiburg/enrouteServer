f1f68f39a86bedcbfac2ef07423aaba6fe9bc8f1
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2022-02-08 07:23:40 +0100

    Skip over identical JSON files

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,6 +2,7 @@
 
 import datetime
 import filecmp
+import geopandas
 import glob
 import json
 import os
@@ -16,15 +17,28 @@ serverURL = "https://cplx.vm.uni-freiburg.de/storage/enroute-GeoJSONv003"
 # Copy files over to the mapStorageDir
 #
 os.chdir('out')
-for file in glob.glob("**/*.geojson", recursive=True)+glob.glob("**/*.mbtiles", recursive=True)+glob.glob("**/*.txt", recursive=True):
-    if filecmp.cmp(file, mapStorageDir+'/'+file, shallow=False):
-        print('Skipping over {}, which is unchanged'.format(file))
+for fileName in glob.glob("**/*.geojson", recursive=True)+glob.glob("**/*.mbtiles", recursive=True)+glob.glob("**/*.txt", recursive=True):
+    stagingFileName = mapStorageDir+'/'+fileName
+    hasChanged = True
+#    if filecmp.cmp(file, mapStorageDir+'/'+file, shallow=False):
+#        hasChanged = False
+    
+    if (fileName.endswith('geojson')):
+        A = json.load( open(fileName) )
+        A['info'] = 'infoString'
+        B = json.load( open(stagingFileName) )
+        B['info'] = 'infoString'
+        if A == B:
+            hasChanged = False
+
+    if hasChanged:
+        print('Zopfli compress {} and move to staging dir'.format(fileName))
+        subprocess.run("rm -f '" + fileName + ".gz'", shell=True, check=True)
+        subprocess.run("zopfli --best '" + fileName + "'", shell=True, check=True)
+        shutil.move(fileName, stagingFileName)
+        shutil.move(fileName+'.gz', stagingFileName+'.gz')
     else:
-        print('Zopfli compress {} and move to staging dir'.format(file))
-        subprocess.run("rm -f '" + file + ".gz'", shell=True, check=True)
-        subprocess.run("zopfli --best '" + file + "'", shell=True, check=True)
-        shutil.move(file, mapStorageDir+'/'+file)
-        shutil.move(file+'.gz', mapStorageDir+'/'+file+'.gz')
+        print('Skipping over {}, which is unchanged'.format(fileName))
 
 
 #
@@ -32,21 +46,21 @@ for file in glob.glob("**/*.geojson", recursive=True)+glob.glob("**/*.mbtiles", 
 #
 print("\nGenerate maps.json")
 maps = []
-for file in glob.glob(mapStorageDir + "/**/*.geojson", recursive=True)+glob.glob(mapStorageDir + "/**/*.mbtiles", recursive=True)+glob.glob(mapStorageDir + "/**/*.txt", recursive=True):
+for fileName in glob.glob(mapStorageDir + "/**/*.geojson", recursive=True)+glob.glob(mapStorageDir + "/**/*.mbtiles", recursive=True)+glob.glob(mapStorageDir + "/**/*.txt", recursive=True):
     map = {}
-    map['path']  = file.replace(mapStorageDir + "/", "")
-    t = os.path.getmtime(file)
+    map['path']  = fileName.replace(mapStorageDir + "/", "")
+    t = os.path.getmtime(fileName)
     d = datetime.datetime.fromtimestamp(t)
     map['time'] = ("%04d" % d.year) + ("%02d" % d.month) + ("%02d" % d.day)
-    map['size'] = os.path.getsize(file)
+    map['size'] = os.path.getsize(fileName)
     maps.append(map)
 
 top = {'maps': maps}
 top['url'] = serverURL
 top['whatsNew'] = 'If you ever move to the south Atlantic, you will be delighted to learn that aviation maps for the <strong>Falkland Islands</strong> are now available.'
-file = open(mapStorageDir + '/maps.json', 'w')
-file.write(json.dumps(top, sort_keys=True, indent=4))
-file.close()
+fileName = open(mapStorageDir + '/maps.json', 'w')
+fileName.write(json.dumps(top, sort_keys=True, indent=4))
+fileName.close()
 
 key = input("Upload maps (y/n)? ")
 if key == "y":
