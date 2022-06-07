f6de8fe5c3154f3996b04769ee696de8ac0e6435
Stefan Kebekus <stefan.kebekus@math.uni-freiburg>
2022-06-07 13:09:39 +0200

    Done with reporting points

--- scripts/generateWorldAviationMap.py
+++ scripts/generateWorldAviationMap.py
@@ -2,22 +2,54 @@
 
 from datetime import date
 
+import geopy.distance
 import json
 import OFMX
 import openAIP2
 
 
 #
+# This generator function does two things. First, it removes all duplicate items
+# from the list. Second, it searches for reporting points that are less than
+# 100m apart.  These duplicate reporting points may enter the list because we
+# import reporting points from openAIP and from open flightmaps.
+#
+# The current implementation is seriously slow and should be improved.
+#
 # Thanks to
 # https://stackoverflow.com/questions/33955225/remove-duplicate-json-objects-from-list-in-python/33955336
 #
 def removeduplicate(it):
+    # This is a set of feature that we have already seen
     seen = set()
+
+    # This is a list of reporting point coordinates that we have already seen.
+    reportingPointCoordinates = []
     for x in it:
+
+        # If the item has been seen, then skip it
         t = json.dumps(x)
-        if t not in seen:
-            yield x
-            seen.add(t)
+        if t in seen:
+            continue
+
+        # If the item is a waypoint whose coordinates are already in the list
+        # (up to 100m tolerance), then also skip it
+        if x['properties']['TYP'] == 'WP':
+            coord = x['geometry']['coordinates']
+            hasRP = False
+            for exCoord in reportingPointCoordinates:
+                if geopy.distance.geodesic(coord, exCoord).km < 0.1:
+                    hasRP = True
+                    break
+            if hasRP:
+                print(x)
+                continue
+            #reportingPointCoordinates.append(coord)
+        
+        # Yield the element, and add it to the list of elements that we have
+        # already seen.
+        yield x
+        seen.add(t)
 
 
 #
@@ -25,8 +57,8 @@ def removeduplicate(it):
 #
 
 features = []
+features += OFMX.readOFMX() # OFMX comes first, because we trust OFMX most
 features += openAIP2.readOpenAIP()
-features += OFMX.readOFMX()
 
 # Remove duplicated entries
 features = list(removeduplicate(features))
