f3fa1d9d1c25d60e7e1091a42f5901ea086236af
Stefan Kebekus <kebekus@users.noreply.github.com>
2024-02-04 16:00:05 +0100

    Add file

--- statistics/logger.py
+++ statistics/logger.py
@@ -1,10 +1,25 @@
 import re
 import pickle
+import sys
 from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-file=open("access.log.1","r")
+#
+# Setup data structures
+#
+
+mapAccessNumbers = {}
+datesAlreadyProcessed = []
+
+try:
+    with open('filename.pickle', 'rb') as f:
+        mapAccessNumbers = pickle.load(f)
+    datesAlreadyProcessed = mapAccessNumbers.keys()
+except:
+    print("Cannot read old data.")
+
+file=open(sys.argv[1],"r")
 
 content = file.read()
 lines = content.splitlines()
@@ -12,33 +27,28 @@ lines = content.splitlines()
 datePattern = r'\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}'
 
 
-mapAccessNumbers = {}
 
 
 count = 0
 for line in lines:
     if "maps.json" not in line:
         continue
+
     # Find the date in the log line
     date = re.search(datePattern, line)
     if not date:
         continue
-
-    count = count + 1
-
     date_string = date.group(0)
     date = datetime.strptime(date_string, "%d/%b/%Y:%H:%M:%S")
-
-    # Convert the datetime object to days since the epoch
     days_since_epoch = (date - datetime(1970, 1, 1)).days
+
+    if days_since_epoch in datesAlreadyProcessed:
+        continue
+
     if days_since_epoch in mapAccessNumbers:
         mapAccessNumbers[days_since_epoch] = mapAccessNumbers[days_since_epoch]+1
     else:
         mapAccessNumbers[days_since_epoch] = 1
-
-mapAccessNumbers[19755] = 800
-mapAccessNumbers[19757] = 850
-
 
 with open('filename.pickle', 'wb') as f:
     pickle.dump(mapAccessNumbers, f)
@@ -47,4 +57,6 @@ fig, ax = plt.subplots()
 ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
 plt.plot(mapAccessNumbers.keys(), mapAccessNumbers.values(), "bo")
 plt.ylabel('Users')
-plt.show()
+#plt.show()
+plt.savefig("test.png")
+plt.close()
