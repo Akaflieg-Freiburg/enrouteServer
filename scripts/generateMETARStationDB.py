a714e412e69b388b4eb1b37186f5ab2e4bc8a53a
Stefan Kebekus <kebekus@users.noreply.github.com>
2024-12-25 18:24:44 +0100

    Update generateMETARStationDB.py

--- scripts/generateMETARStationDB.py
+++ scripts/generateMETARStationDB.py
@@ -3,103 +3,54 @@
 import os
 
 """
-flarmDB
+METAR Station DB
 ====================================
-Toolset to create a compact database the maps Flarm IDs to aircraft callsigns.
+Toolset to create a list of METAR stations known to the aviation weather center.
+
+This script reads the list of known METAR stations from the aviation weather
+center and writes a plain text file in ISO-8859-1 encoding ("Latin1"). The first
+line is a short description of the content. Afterwards, starting from byte #xxx,
+there are about 10,000 entries, each of which is exactly 5 characters long. The
+entries are sorted in alphabetical order. The file is written to the "out"
+directory. If the directory does not exist, it is created.
 """
 
 from datetime import date
 import gzip
 import json
-import re
 import requests
-
-
-def A(dictionary):
-    """Read a Flarmnet database in fln format (as also used by
-    LXNavigation/XCSoar, WinPilot, LK8000, ClearNav).  Files of this form can be
-    downloaded from Flarmnet, at this url:
-    https://www.flarmnet.org/static/files/wfn/data.fln The method fills a
-    dicitionary that maps Flarm IDs to aircraft callsigns. Input data where the
-    'callsign' field is not obviously a well-defined callsign are ignored. This
-    function will raise an exception on error
-
-    :param inFileName: Name of input file
-
-    :param dictionary: Dictionary to which the results will be appended
-
-    :returns: String that describes the input file version
-
-    """
-
-    # Regular expression to check if a callsign is valid
-    validReg = re.compile('^([A-Z0-9]{1,2}-[A-Z0-9]*|N[A-Z0-9]{2,6})$')
-
-    try:
-        response = requests.get('https://aviationweather.gov/data/cache/stations.cache.json.gz')
-        response.raise_for_status()
-        # Code here will only run if the request is successful
-    except requests.exceptions.HTTPError as errh:
-        print(errh)
-        exit(-1)
-    except requests.exceptions.ConnectionError as errc:
-        print(errc)
-        exit(-1)
-    except requests.exceptions.Timeout as errt:
-        print(errt)
-        exit(-1)
-    except requests.exceptions.RequestException as err:
-        print(err)
-        exit(-1)
-    decompressed_data = gzip.decompress(response.content)
-    response.encoding = 'utf-8'
-    lines = response.text.splitlines()
-    stations = json.loads(decompressed_data)
-    for station in stations:
-        print(station['icaoId'])
-    return "Flarmnet Data version "+version
-
-
-
-
-def readMETARStationDB(outFileName):
-    """Read a Flarmnet and an OGN database and combines the data. The method
-    writes a plain text file in ISO-8859-1 encoding ("Latin1"). The first line
-    is a short description of the content. Every following line is exactly 24
-    bytes long.
-
-    - Bytes  0 -  5 of the line contain the Flarm ID
-    - Bytes  7 - 22 of the line contain the aircraft callsign, left justified
-      and filled with space
-
-    The lines in the output file are sorted alphabetically by Flarm ID.
-
-    This function will raise an exception on error
-
-    :param flarmnetFileName: Name of input file
-
-    :param ognFileName: Name of input file
-
-    :param outFileName: Name of output file
-
-    """
-
-    result = {}
-
-    flarmnetversion = A(result)
-    version = "METAR Station Database. Compiled from \n"
-
-    outfile = open(outFileName, 'w', encoding='ISO-8859-1')
-    outfile.write(version)
-    for FLARMID, callsign in sorted(result.items()):
-        outfile.write(FLARMID)
-        outfile.write(",")
-        outfile.write(callsign.ljust(16))
-        outfile.write("\n")
 
 
 #
 # Main function: run the methods of this module on test data
 #
 os.makedirs('out', exist_ok=True)
-readMETARStationDB('out/METAR Stations.txt')
+
+try:
+    response = requests.get('https://aviationweather.gov/data/cache/stations.cache.json.gz')
+    response.raise_for_status()
+    # Code here will only run if the request is successful
+except requests.exceptions.HTTPError as errh:
+    print(errh)
+    exit(-1)
+except requests.exceptions.ConnectionError as errc:
+    print(errc)
+    exit(-1)
+except requests.exceptions.Timeout as errt:
+    print(errt)
+    exit(-1)
+except requests.exceptions.RequestException as err:
+    print(err)
+    exit(-1)
+decompressed_data = gzip.decompress(response.content)
+stations = json.loads(decompressed_data)
+res = []
+for station in stations:
+    if 5 < len(station['icaoId']):
+        continue
+    res.append(station['icaoId'].ljust(5))
+res.sort()
+
+outfile = open("out/METAR Stations.txt", 'w', encoding='ISO-8859-1')
+outfile.write("METAR Station Database. Compiled from data provided by the Aviation Weather Center on " + date.today().strftime("%d/%m/%Y") + "\n")
+outfile.write("".join(res))
