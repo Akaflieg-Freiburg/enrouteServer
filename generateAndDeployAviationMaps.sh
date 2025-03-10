e068da343049cb374ac9e8f55ae44f21cbb27555
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-03-10 07:53:40 +0100

    Update generateAndDeployAviationMaps.sh

--- generateAndDeployAviationMaps.sh
+++ generateAndDeployAviationMaps.sh
@@ -8,6 +8,7 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd scripts
+python3 ./generateWorldRasterMaps.py
 python3 ./generateWorldAviationMap.py
 python3 ./splitAviationMap.py
 python3 ./deploy-hetzner.py
