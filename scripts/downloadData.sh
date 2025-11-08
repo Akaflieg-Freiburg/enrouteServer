c46d6172b5f48c567936e21dcd874a36cfe7c919
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-11-08 13:11:21 +0100

    Update downloadData.sh

--- scripts/downloadData.sh
+++ scripts/downloadData.sh
@@ -12,28 +12,28 @@ cd ..
 
 mkdir data/ne_10m_admin_0_countries
 cd data/ne_10m_admin_0_countries
-wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip
+wget https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_countries.zip
 unzip ne_10m_admin_0_countries.zip
 rm ne_10m_admin_0_countries.zip
 cd ../..
 
 mkdir data/ne_10m_antarctic_ice_shelves_polys
 cd data/ne_10m_antarctic_ice_shelves_polys
-wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_antarctic_ice_shelves_polys.zip
+wget https://naciscdn.org/naturalearth/10m/physical/ne_10m_antarctic_ice_shelves_polys.zip
 unzip ne_10m_antarctic_ice_shelves_polys.zip
 rm ne_10m_antarctic_ice_shelves_polys.zip
 cd ../..
 
 mkdir data/ne_10m_glaciated_areas
 cd data/ne_10m_glaciated_areas
-wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_glaciated_areas.zip
+wget https://naciscdn.org/naturalearth/10m/physical/ne_10m_glaciated_areas.zip
 unzip ne_10m_glaciated_areas.zip
 rm ne_10m_glaciated_areas.zip
 cd ../..
 
 mkdir data/ne_10m_urban_areas
 cd data/ne_10m_urban_areas
-wget https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_urban_areas.zip
+wget https://naciscdn.org/naturalearth/10m/cultural/ne_10m_urban_areas.zip
 unzip ne_10m_urban_areas.zip
 rm ne_10m_urban_areas.zip
 cd ../..
